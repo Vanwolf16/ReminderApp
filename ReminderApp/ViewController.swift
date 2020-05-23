95d1d7c191ad051fc4c2788f7f740fb9a8626791
David Murillo <d.murillo28@yahoo.com>
2020-05-23 11:46:44 -0700

    Initial Commit

---
+++ ReminderApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReminderApp
+//
+//  Created by David Murillo on 5/23/20.
+//  Copyright © 2020 MuriTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
